146c40d67bc605a26fadfc0cb89308d5afcd64be
Albertoxfr <carlosfreitez27937518@gmail.com>
2023-02-09 01:07:30 -0400

    Numeros

---
+++ Funciones/Numeros.js
@@ -0,0 +1,10 @@
+let altura = 170
+let alturaFloat = 1.70
+let peso = 67.8
+console.log(`Mi altura redondeanda hacia arriba es ${Math.ceil(altura)}`)
+
+console.log(`Mi peso redondeado hacia abajo es ${Math.floor(peso)}`)
+
+let max_valor_JS = Number.MAX_VALUE
+
+console.log(max_valor_JS === max_valor_JS + 1)
