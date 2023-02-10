c816c761849ffc0dadf969009f93583c14fd0501
Albertoxfr <carlosfreitez27937518@gmail.com>
2023-02-10 00:11:09 -0400

    Cambio 5

---
+++ Funciones/Sintaxis.js
@@ -0,0 +1,18 @@
+let Nombre = "Carlos"
+let edad = 21
+
+if (true) {
+    console.log("Si soy programador")
+} else {
+    console.log("No soy programador")
+}
+
+let fecha = new Date("2001, 3, 19")
+console.log(fecha)
+
+const libroFavorito = {
+    titulo: "Harry Potter y la piedra filosofal",
+    autor: "J. K. Rowling",
+    fecha: 26-06-1997,
+    url: "https://es.wikipedia.org/wiki/Harry_Potter_y_la_piedra_filosofal"
+}
